62609b4f19b8fdfa732bb24806ffd2078cc5096d
Diana Ovechkina <ovechkinadiana@yandex.ru>
2022-02-18 17:05:33 +0300

    Initial Commit

---
+++ ChangeViewColor/ChangeViewColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeViewColorApp.swift
+//  ChangeViewColor
+//
+//  Created by Diana Ovechkina on 18.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ChangeViewColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
